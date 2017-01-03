78c21143eb8fd83e3d7ff947934abc3b9476c809
Marco Patierno <marco_patierno@msn.com>
2017-01-03 11:26:23 +0100

    other test

--- bot-one-shot.js
+++ bot-one-shot.js
@@ -1,7 +1,7 @@
 var Twit = require('twit');
 var T = new Twit(require('./settings'));
 
-T.get('lists/members', { slug: 'test'}, function(err, data, response) {
+T.get('lists/members', { 'slug': 'test', 'owner_screen_name' : 'MPBotterinojs'}, function(err, data, response) {
     console.log('error ' + err);
     console.log('data ' + data);
     console.log('response ' + response);
