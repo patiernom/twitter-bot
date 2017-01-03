cb6c9dd77ee50b06750d035f1d51acbf55e2c38a
Marco Patierno <marco_patierno@msn.com>
2017-01-03 10:35:53 +0100

    change dependecies

---
+++ bot.js
@@ -0,0 +1,20 @@
+var Twit = require('twit');
+var TwitterBot = require('node-twitterbot').TwitterBot;
+var Bot = new TwitterBot(require('./settings'));
+var phraseArray = [ "hey twitter",
+    "im tweeting",
+    "tweet tweet",
+    "tweetstorm time... 1/22",
+    "plz RT v important",
+    "delete ur account",
+    "it me",
+    "same",
+    "#dogpants go on 4 legs!!",
+    "#thedress is blue and black" ];
+
+function chooseRandom(myArray) {
+    return myArray[Math.floor(Math.random() * myArray.length)];
+}
+
+var phrase = chooseRandom(phraseArray) + ", " + chooseRandom(phraseArray);
+Bot.tweet(phrase);
