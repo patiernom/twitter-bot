9afc2e7c57fefbe3255aa04c096e9fc6c6ad32e3
Marco Patierno <marco_patierno@msn.com>
2017-01-03 11:28:42 +0100

    chnge console log

--- bot-one-shot.js
+++ bot-one-shot.js
@@ -2,8 +2,13 @@ var Twit = require('twit');
 var T = new Twit(require('./settings'));
 
 T.get('lists/members', { 'slug': 'test', 'owner_screen_name' : 'MPBotterinojs'}, function(err, data, response) {
-    console.log('error ' + err);
-    console.log('data ' + data);
-    console.log('response ' + response);
+    if (err) {
+        console.log('error ' + err);
+    } else {
+        console.log('data: ');
+        console.dir(data);
+        console.log('response: ');
+        console.dir(response);
+    }
 });
 
