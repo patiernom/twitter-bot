cf3827daf71ba20c048c8c50040ee62f673039be
Marco Patierno <marco_patierno@msn.com>
2017-01-03 12:02:57 +0100

    change worker

--- bot-one-shot.js
+++ bot-one-shot.js
@@ -1,14 +1,41 @@
-var Twit = require('twit');
-var T = new Twit(require('./settings'));
+var Twit = require('twit'),
+    T = new Twit(require('./settings')),
+    R = require('ramda'),
+    getUserData = function(user){
+        "use strict";
+
+        return {
+            id: user.id,
+            name: user.screen_name
+        }
+    },
+    generateTweetMessage = function(user) {
+        "use strict";
+
+        return "Ciao @" + user.name + " come stai? Mi sembri in ottima forma nonsotante i pranzi e le cene delle feste!";
+    },
+    generateTweetPost = function(reply) {
+        "use strict";
+
+        T.post('statuses/update', { status: reply }, function(err, data, response) {
+            console.log(data);
+        })
+    },
+    generateTweet = function(user) {
+        "use strict";
+
+        generateTweetPost(generateTweetMessage(user));
+    };
 
 T.get('lists/members', { 'slug': 'test', 'owner_screen_name' : 'MPBotterinojs'}, function(err, data, response) {
     if (err) {
         console.log('error ' + err);
     } else {
-        console.log('data: ');
-        console.dir(data.users);
-        /*console.log('response: ');
-        console.dir(response);*/
+        var usersList = R.map(getUserData, data.users);
+
+        console.dir(usersList);
+
+        R.forEach(generateTweet, usersList);
     }
 });
 
