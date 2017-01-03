2085d12065b757a4186602f11158a9bc2a1726bf
Marco Patierno <marco_patierno@msn.com>
2017-01-03 15:55:05 +0100

    add upload media

---
+++ bot-one-shot-with-image.js
@@ -0,0 +1,67 @@
+var fs = require('fs'),
+    Twit = require('twit'),
+    T = new Twit(require('./settings')),
+    R = require('ramda'),
+    b64content = fs.readFileSync('test.jpg', { encoding: 'base64' });
+    getUserData = function(user){
+        "use strict";
+
+        return {
+            id: user.id,
+            name: user.screen_name
+        }
+    },
+    generateTweetMessage = function(user, mediaIdStr) {
+        "use strict";
+
+        return {
+            status: "@" + user.name + " come stai? Mi sembri in ottima forma nonsotante i pranzi e le cene delle feste!",
+            media_ids: [mediaIdStr]
+        };
+    },
+    generateTweetPost = function(status) {
+        "use strict";
+
+        T.post('statuses/update', status, function(err, data, http_response) {
+            if (err) {
+                console.log('error ' + err);
+            } else {
+                console.log(data);
+            }
+        });
+    },
+    generateTweet = function(media){
+        return function(user) {
+            "use strict";
+
+            generateTweetPost(generateTweetMessage(user, media));
+        }
+    };
+
+T.post('media/upload', { media_data: b64content }, function (err, data, http_response) {
+    var mediaIdStr = data.media_id_string,
+        altText = "this is a test image",
+        meta_params = {
+            media_id: mediaIdStr,
+            alt_text: { text: altText }
+        };
+
+    T.post('media/metadata/create', meta_params, function (err, data, http_response) {
+        if (!err) {
+
+            T.get('lists/members', { 'slug': 'test', 'owner_screen_name' : 'MPBotterinojs'}, function(err, data, http_response) {
+                if (err) {
+                    console.log('error ' + err);
+                } else {
+                    var usersList = R.map(getUserData, data.users);
+
+                    console.dir(usersList);
+
+                    R.forEach(generateTweet(mediaIdStr), usersList);
+                }
+            });
+        }
+    })
+});
+
+
